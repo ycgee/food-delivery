3f09e6c6ef53904bb0e688acdcace62158dba2ae
ycgee <youngsteel@gmail.com>
2022-11-12 12:00:50 +0900

    commit

---
+++ customer/src/main/java/food/delivery/domain/Paid.java
@@ -0,0 +1,12 @@
+package food.delivery.domain;
+
+import food.delivery.infra.AbstractEvent;
+import lombok.Data;
+import java.util.*;
+
+@Data
+public class Paid extends AbstractEvent {
+
+    private Long id;
+    private String orderId;
+}
---
+++ front/src/main/java/food/delivery/domain/Paid.java
@@ -0,0 +1,21 @@
+package food.delivery.domain;
+
+import food.delivery.domain.*;
+import food.delivery.infra.AbstractEvent;
+import java.util.*;
+import lombok.*;
+
+@Data
+@ToString
+public class Paid extends AbstractEvent {
+
+    private Long id;
+    private String orderId;
+
+    public Paid(Payment aggregate){
+        super(aggregate);
+    }
+    public Paid(){
+        super();
+    }
+}
